0ce24171896cf536b501633a078342314728f9d0
Andy Filchev <andyfilchev@protonmail.com>
2020-10-09 17:17:32 +0300

    feat: Update /dicover-sofia page

--- src/Projects/DiscoverSofia.elm
+++ src/Projects/DiscoverSofia.elm
@@ -96,9 +96,12 @@ articleText = """
 
 ## About
 
-The **Discover Sofia** project was my Bachelor Thesis. The main idea was to explore the Web-browser's capabilties as a platform for Machine Learning. The current implementation is far from complete but for now it allows the user to:
+The **Discover Sofia** project was my Bachelor Thesis. The main idea was to 
+explore the Web-browser's capabilties as a platform for Machine Learning. 
+The current implementation is far from complete but for now it allows the user to:
 
-- Use Image Recognition to recognize particular landmark by using the video feed from the back camera or by uploading a photo
+- Use Image Recognition to recognize particular landmark by using the 
+video feed from the back camera or by uploading a photo
 - Check interesting landmarks on the map and get short information about them
 - Get basic Navigation to the selected landmark
 
@@ -124,7 +127,8 @@ The implementation is split in three parts:
 
 #### Machine Learning
 
-Transfer Learning is used to improve the overall accuracy of the model since the size of the photos set is extremely small. In this case the 
+Transfer Learning is used to improve the overall accuracy of the 
+model since the size of the photos set is extremely small.
 
 ![accuracy and loss of the model](/z-context/img/projects/discover-sofia/model-accuracy.png)
 
@@ -136,15 +140,23 @@ Post-training quantization is used to reduce the model size.
 
 The interface is devided in three pages:
 
-1) **Map**:Represents an interactive map with visualized landmarks. Choosing a certain landmark shows the user a basic info about the landmark and an option for basic navigation. 
+1) **Map**:Represents an interactive map with visualized landmarks. 
+Choosing a certain landmark shows the user a basic info about the landmark 
+and an option for basic navigation. 
 
-2) **Camera**: Represents an interface which shows the video feed from the back camera if there is one. If there is back camera the video feed is used for inference using the Image Recognition model. The result is displayed below the video element.
+2) **Camera**: Represents an interface which shows the video feed from the 
+back camera if there is one. If there is back camera the video feed is used 
+for inference using the Image Recognition model. The result is displayed below the video element.
 
-3) **Photo**: Represents an interface for uploading photo from the device. The uploaded photo is then used for inference using the Image Recognitionn model and the result is displayed below the photo.
+3) **Photo**: Represents an interface for uploading photo from the device.
+ The uploaded photo is then used for inference using the Image Recognitionn 
+ model and the result is displayed below the photo.
 
 #### Integrating the Image Recognition Model with the UI
 
-Elm Ports are used for communication bewteen the UI and Image Recognition model. The logic for working with the model and preparing the images for inference is implemented with the help of the TensorFlow.js library.
+Elm Ports are used for communication bewteen the UI and Image Recognition model. 
+The logic for working with the model and preparing the images for inference is
+ implemented with the help of the TensorFlow.js library.
 
 ![communication bewteen the UI and TF.js](/z-context/img/projects/discover-sofia/app-comms.png)
 
@@ -158,7 +170,8 @@ Two external APIs are used for implementing the interactive map:
 
 2) HereMaps API
 
-> Used to display the map and vizualize the landmarks on it. Also provideing the basic navigation features.
+> Used to display the map and vizualize the landmarks on it. 
+Also provideing the basic navigation features.
 
 ***
 
@@ -171,21 +184,23 @@ Two external APIs are used for implementing the interactive map:
 1) Download or Clone the repository
 
 2) Install the needed dependecies
-    - elm 
-    - elm-live
-    - cors-anywhere
+* elm 
+* elm-live
+* cors-anywhere
 
-3) Start `cors-anywhere`. From the installation directory of `cors-anywhere` issue the following command in a terminal
+3) Start `cors-anywhere`. From the installation directory of `cors-anywhere` issue the 
+following command in a terminal:
     
-    ```
-    node server.js
-    ```
+```
+node server.js
+```
 
-4) Start the local server. First checkout to the `development` branch. Then from the base of the project directory issue the following command:
+4) Start the local server. First checkout to the `development` branch. Then from the 
+base of the project directory issue the following command:
 
-    ```
-    elm-live src/Main.elm --pushstate -- --debug --output =elm.js
-    ```
+```
+elm-live src/Main.elm --pushstate -- --debug --output =elm.js
+```
 
 ***
 
@@ -222,15 +237,19 @@ Elm dependencies:
 
 There is much to be done: 
 
-* Use the inference result so that the user can be redirected to Map page showing the basic information about the landmark. To do this it should be analized how and where to do it.
+* Use the inference result so that the user can be redirected to the Map page
+ showing the basic information about the landmark. To do this it should be analized how 
+ and where to do it.
 * Implement back-end server and transform the app to PWA
 * Making the experience more interactive
 * Update the model architecture from MobilenetV2 to MobilenetV3
-* Try further optimizing the inference times by using smaller input images. Currently using [224x224]
+* Try further optimizing the inference times by using smaller input images. 
+Currently using [224x224]
 
 ***
 
 ## Issues
 
-* In some versions of Firefox the GPU backend could not be used. Looks like an issue with Firefox and not TensorFlow.js.
+* In some versions of Firefox the GPU backend could not be used. Looks like an issue 
+with Firefox and not TensorFlow.js.
 """
